87d0188432d2b838822cfbe7bf120c1d179bf03d
mwendaB <brian.mwenda@moringaschool.com>
2021-09-21 18:48:06 +0300

    finished with js

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,3 +42,35 @@ const isValidEmail = (email) => {
   const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
   return re.test(String(phone).toLowerCase());
   };
+
+
+  let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+console.log("we are here");
+if (!shouldValidate) return;
+
+isFormValid = true;
+inputFields.forEach((input) => {
+input.classList.remove("invalid");
+input.nextElementSibling.classList.add("hide");
+
+if (!input.isValid()) {
+input.classList.add("invalid");
+isFormValid = false;
+input.nextElementSibling.classList.remove("hide");
+}
+});
+};
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+shouldValidate = true;
+validateInputs();
+if (isFormValid) {
+
+}
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
