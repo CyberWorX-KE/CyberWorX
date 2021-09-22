a80eba81a15a733b60580f4a952d7b9d1f66776b
RoySamson-stack <rsam9009@gmail.com>
2021-09-22 20:10:11 +0300

    adding the images to the porgram directory

--- assets/js/programdirectory.js
+++ assets/js/programdirectory.js
@@ -1,35 +1,32 @@
+var filterInput = document.getElementById("search");
+const head = document.querySelectorAll('tbody tr');
 
-  var filterInput = document.getElementById("search");
-  const head = document.querySelectorAll('tbody tr');
+filterInput.addEventListener('keyup', function (event) {
+  const q = event.target.value;
+  head.forEach(head => {
+    head.querySelector('th').textContent.toLowerCase().startsWith(q) ?
+      head.style.display = '' :
+      head.style.display = 'none'
+  })
+});
 
-  filterInput.addEventListener('keyup', function (event) {
-    const q = event.target.value;
-    head.forEach(head =>{
-      head.querySelector('th').textContent.toLowerCase().startsWith(q)
-       ? head.style.display = ''
-      : head.style.display = 'none'
-    })
-  });
-
- var check = document.querySelectorAll('.checkbox');
- const table = document.querySelectorAll('tbody tr');
+var check = document.querySelectorAll('.checkbox');
+const table = document.querySelectorAll('tbody tr');
 
 for (i = 0; i < check.length; i++) {
-  check[i].addEventListener("change", (e) =>{
+  check[i].addEventListener("change", (e) => {
     e.preventDefault();
     const filter = e.target.dataset.filter;
     console.log(filter)
     table.forEach((product) => {
-      if(filter == "all"){
+      if (filter == "all") {
         product.style.display = ""
-      }else{
-        if (product.classList.contains(filter))
-        {
-          product.style.display = ""
-        }else{
-          product.style.display = "none"
-        }
+      } else if (product.classList.contains(filter)) {
+        product.style.display = ""
+      } else {
+        product.style.display = "none"
       }
+
     })
   })
 }
