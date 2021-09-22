c8a0580fe2a56912c2f2816e110df9636633bca0
RoySamson-stack <rsam9009@gmail.com>
2021-09-22 15:13:23 +0300

    code cleanup

--- assets/js/programdirectory.js
+++ assets/js/programdirectory.js
@@ -1,28 +1,4 @@
-  // $("#search").bind("keyup", function () {
-  //   var text = $(this).val().toLowerCase();
-  //   var program = $("#program");
 
-  //   //first, hide all:
-  //   program.parent().hide();
-
-  //   //show only those matching user input:
-  //   program.filter(function () {
-  //     return $(this).text().toLowerCase().indexOf(text) == 0;
-  //   }).parent().show();
-  // });
-  // $("form.filter").on('click', function (event) {
-  //   event.preventDefault();
-  //   var comp = new FormData(this);
-  //   const filter = {
-  //     bank: comp.get('bank'),
-  //     telco: comp.get("telco"),
-  //     sacco: comp.get("sacco"),
-  //   }
-  //   console.log(filter)
-  //   if (filter[comp.get('bank')].checked === true) {
-  //     alert('checked')
-  //   }
-  // })
   var filterInput = document.getElementById("search");
   const head = document.querySelectorAll('tbody tr');
 
@@ -33,4 +9,6 @@
        ? head.style.display = ''
       : head.style.display = 'none'
     })
-  })
+  });
+
+ 
