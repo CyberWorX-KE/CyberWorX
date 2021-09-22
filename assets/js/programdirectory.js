e3729406ac9538bf423fe38ede16ddffd2d49597
RoySamson-stack <rsam9009@gmail.com>
2021-09-22 14:48:03 +0300

    adding functinality to the search bar

--- assets/js/programdirectory.js
+++ assets/js/programdirectory.js
@@ -1,12 +1,36 @@
-$("#search").bind("keyup", function() {
-  var text = $(this).val().toLowerCase();
-  var program = $("#program");
+  // $("#search").bind("keyup", function () {
+  //   var text = $(this).val().toLowerCase();
+  //   var program = $("#program");
 
-  //first, hide all:
-  program.parent().hide();
+  //   //first, hide all:
+  //   program.parent().hide();
 
-  //show only those matching user input:
-  program.filter(function () {
-      return $(this).text().toLowerCase().indexOf(text) == 0;
-  }).parent().show();
-});
+  //   //show only those matching user input:
+  //   program.filter(function () {
+  //     return $(this).text().toLowerCase().indexOf(text) == 0;
+  //   }).parent().show();
+  // });
+  // $("form.filter").on('click', function (event) {
+  //   event.preventDefault();
+  //   var comp = new FormData(this);
+  //   const filter = {
+  //     bank: comp.get('bank'),
+  //     telco: comp.get("telco"),
+  //     sacco: comp.get("sacco"),
+  //   }
+  //   console.log(filter)
+  //   if (filter[comp.get('bank')].checked === true) {
+  //     alert('checked')
+  //   }
+  // })
+  var filterInput = document.getElementById("search");
+  const head = document.querySelectorAll('tbody tr');
+
+  filterInput.addEventListener('keyup', function (event) {
+    const q = event.target.value;
+    head.forEach(head =>{
+      head.querySelector('th').textContent.toLowerCase().startsWith(q)
+       ? head.style.display = ''
+      : head.style.display = 'none'
+    })
+  })
